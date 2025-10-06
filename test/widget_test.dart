1c5db862eec09a4a89ec83d7fb633896accc6aa4
Shadman <shadmansakib1010@gmail.com>
2025-10-07 00:38:31 +0600

    test: isolate widget test with fake providers (no Hive)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,33 +1,31 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:job_portal/providers/auth_provider.dart';
-import 'package:job_portal/providers/job_list_provider.dart';
-import 'package:job_portal/providers/saved_jobs_provider.dart';
 import 'package:job_portal/screens/auth/login_screen.dart';
+
+class FakeAuthProvider extends ChangeNotifier {
+  bool get isLoggedIn => false;
+  String? get currentEmail => null;
+}
+
+class FakeSavedJobsProvider extends ChangeNotifier {
+  int get count => 0;
+}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   testWidgets('App starts on Login screen', (tester) async {
-    // Initialize Hive in memory for tests
-    await Hive.initFlutter();
-    await AuthProvider.init();
-
     await tester.pumpWidget(
       MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (_) => AuthProvider()),
-          ChangeNotifierProvider(create: (_) => JobListProvider()),
-          ChangeNotifierProvider(create: (_) => SavedJobsProvider()),
+          ChangeNotifierProvider(create: (_) => FakeAuthProvider()),
+          ChangeNotifierProvider(create: (_) => FakeSavedJobsProvider()),
         ],
         child: const MaterialApp(home: LoginScreen()),
       ),
     );
 
-  // First pump builds widgets; second pump lets post-frame callbacks run
-  await tester.pump();
-  await tester.pump();
+    await tester.pump();
 
     expect(find.text('Login'), findsOneWidget);
     expect(find.text('Create account'), findsOneWidget);
